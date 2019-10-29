d07638c1ed2777ce21673806a429eed751a27168
roger <roger@DESKTOP-EMLM8C3>
2019-10-29 07:50:56 -0300

    desafio 34

--- Algoritmo_Programacao/src/Lista_Exercicios_N2/Exercicio_34_Desafio_repeticao_N2.java
+++ Algoritmo_Programacao/src/Lista_Exercicios_N2/Exercicio_34_Desafio_repeticao_N2.java
@@ -16,38 +16,46 @@ public class Exercicio_34_Desafio_repeticao_N2 {
 
 		do {
 			System.out.println();
-			System.out.printf("Informe a matrícula do %do(a) vendedor(a): ", cont + 1);
+			System.out.printf("Informe a matrícula do %do(a) vendedor(a):..........", cont + 1);
 			matricula = teclado.nextInt();
-			System.out.printf("Informe o número de carros vendidos: ");
+			System.out.printf("Informe o número de carros vendidos: ..............");
 			numCarros = teclado.nextInt();
 
 			for (i = 1; i <= numCarros; i++) {
-				System.out.printf("Informe o valor do %do carro: ", i);
+				System.out.printf("Informe o valor do %do carro: ......................", i);
 				valorCarro = teclado.nextDouble();
 				valorTotalCarros += valorCarro;
 			}
 
-			if (numCarros > 0 && numCarros <= 10) {
+			if (numCarros <= 10 && valorTotalCarros < 50.000) {
 				comissaoTotal = numCarros * comissao1;
 				salarioTotal = salarioFixo + comissaoTotal;
-			} else if (numCarros > 10) {
-				comissaoTotal = numCarros * comissao2;
-				salarioTotal = salarioFixo + comissaoTotal;
-
 			}
-			if (valorTotalCarros > 50.000) {
+			else if (numCarros <= 10 && valorTotalCarros > 50.000) {
+				comissaoTotal = numCarros * comissao1;
 				bonificacao = valorTotalCarros * percBonificacao;
 				salarioTotal = salarioFixo + comissaoTotal + bonificacao;
 
 			}
+			else if (numCarros > 10 && valorTotalCarros < 50.000) {
+				comissaoTotal = numCarros * comissao2;
+				salarioTotal = salarioFixo + comissaoTotal;
+			}
+			else if (numCarros > 10 && valorTotalCarros > 50.000) {
+				comissaoTotal = numCarros * comissao2;
+				bonificacao = valorTotalCarros * percBonificacao;
+				salarioTotal = salarioFixo + comissaoTotal + bonificacao;
+
+			}
+
 			System.out.println();
-			System.out.printf("Valor do salario fixo: %.2f\n", salarioFixo);
-			System.out.printf("Valor da comissão total: %.2f\n", comissaoTotal);
+			System.out.printf("Valor do salario fixo:..................................%.2f\n", salarioFixo);
+			System.out.printf("Valor total dos carros vendidos: .......................%.2f\n", valorTotalCarros);
+			System.out.printf("Valor da comissão total: ...............................%.2f\n", comissaoTotal);
 			System.out.printf("Valor da bonificação: %.2f\n", bonificacao);
-			System.out.printf("O salário total do(a) vendedor(a) sob matrícula %d : R$ %.2f\n", matricula,
-					salarioTotal);
+			System.out.printf("Salário total do(a) vendedor(a) sob matrícula %d : R$ %.2f\n", matricula, salarioTotal);
 			System.out.println();
-			System.out.printf("Calcular vendas do próximo vendedor? 1-Sim 2-Não ");
+			System.out.printf("Calcular vendas do próximo vendedor? 1-Sim 2-Não.........");
 			resp = teclado.nextInt();
 			cont++;
 		} while (resp == 1);
